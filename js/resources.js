ce02e993c7942a69ee2e4a1a31662cf104a34a3a
Eloisa <grrrero@users.noreply.github.com>
2018-02-27 17:03:13 -0500

    FLOE-540: Refactored, added content

--- js/resources.js
+++ js/resources.js
@@ -2,45 +2,41 @@ $(document).ready(function(){
 
   // Expand all
   $( '.floe-resources-show' ).click(function() {
-      $( '.arrow' ).text('\u25BC');
-      $( '.floe-resources-content' ).show(function() {
-        $('.floe-resources .floe-resources-category a').css({ 'background-color': '#fccd00', 'color': '#000000' }).hover(function() {
-          $(this).css({ 'background-color': '#000000', 'color': '#fccd00' });
-          }).mouseout(function() {
-          $(this).css({ 'background-color': '#fccd00', 'color': '#000000' });
-        });
-      });
+      $('.arrow').text('\u25BC');
+      $( '.floe-resources-toggle' )
+        .attr('aria-expanded', true)
+        .closest('.floe-resources-category')
+        .find('ul')
+        .toggleClass('floe-resources-content-show', true);
     });
 
-
-  //   // Expand all
-  // $( '.floe-resources-show' ).click(function() {
-  //     $( '.arrow' ).text('\u25BC');
-  //     $( '.floe-resources-content' ).show(function() {
-  //       $('.floe-resources .floe-resources-tab a').css({ 'background-color': '#fccd00', 'color': '#000000' }).hover(function() {
-  //         $(this).css({ 'background-color': '#000000', 'color': '#fccd00' });
-  //         }).mouseout(function() {
-  //         $(this).css({ 'background-color': '#fccd00', 'color': '#000000' });
-  //       });
-  //     });
-  //   });
-
-  // Hide all
-  $( '.floe-resources-hide' ).click(function() {
+  // // Hide all
+    $( '.floe-resources-hide' ).click(function() {
       $( '.arrow' ).text('\u25B6');
-      $( '.floe-resources-content' ).hide(function() {
-        $('.floe-resources .floe-resources-category a').css({ 'background-color': '#E6E6E6', 'color': '#000000' }).hover(function() {
-          $(this).css({ 'background-color': '#000000', 'color': '#fccd00' });
-          }).mouseout(function() {
-          $(this).css({ 'background-color': '#E6E6E6', 'color': '#000000' });
-        });
-      });
+      $( '.floe-resources-toggle' )
+        .attr('aria-expanded', false)
+        .closest('.floe-resources-category')
+        .find('ul')
+        .toggleClass('floe-resources-content-show', false);
     });
-
 
   // Toggle each category to show content
-  $('.floe-resources-category').click(function() {
-    $('.floe-resources-content').toggleClass('floe-resources-content-show');
+  $('.floe-resources-toggle').click(function(evt) {
+    var target = $(evt.delegateTarget);
+    var state = target.attr('aria-expanded') === 'true' ? true : false;
+    target.attr('aria-expanded', !state)
+      .closest('.floe-resources-category')
+      .find('ul')
+      .toggleClass('floe-resources-content-show', !state);
+      evt.preventDefault();
   });
 
 });
+
+
+    // if (!state) {
+    //   $(.arrow).text('\u25B6');
+    //   else {
+    //   $(.arrow).text('\u25BC');
+    //   }
+    // }
