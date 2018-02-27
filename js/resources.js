8f5c7101d75e4636b6c3937a62bc5945e9b98836
Eloisa <grrrero@users.noreply.github.com>
2018-02-27 14:03:59 -0500

    FLOE-540: Enabled toggling for each category

--- js/resources.js
+++ js/resources.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   $( '.floe-resources-show' ).click(function() {
       $( '.arrow' ).text('\u25BC');
       $( '.floe-resources-content' ).show(function() {
-        $('.floe-resources .floe-resources-tab a').css({ 'background-color': '#fccd00', 'color': '#000000' }).hover(function() {
+        $('.floe-resources .floe-resources-category a').css({ 'background-color': '#fccd00', 'color': '#000000' }).hover(function() {
           $(this).css({ 'background-color': '#000000', 'color': '#fccd00' });
           }).mouseout(function() {
           $(this).css({ 'background-color': '#fccd00', 'color': '#000000' });
@@ -12,11 +12,24 @@ $(document).ready(function(){
       });
     });
 
+
+  //   // Expand all
+  // $( '.floe-resources-show' ).click(function() {
+  //     $( '.arrow' ).text('\u25BC');
+  //     $( '.floe-resources-content' ).show(function() {
+  //       $('.floe-resources .floe-resources-tab a').css({ 'background-color': '#fccd00', 'color': '#000000' }).hover(function() {
+  //         $(this).css({ 'background-color': '#000000', 'color': '#fccd00' });
+  //         }).mouseout(function() {
+  //         $(this).css({ 'background-color': '#fccd00', 'color': '#000000' });
+  //       });
+  //     });
+  //   });
+
   // Hide all
   $( '.floe-resources-hide' ).click(function() {
       $( '.arrow' ).text('\u25B6');
       $( '.floe-resources-content' ).hide(function() {
-        $('.floe-resources .floe-resources-tab a').css({ 'background-color': '#E6E6E6', 'color': '#000000' }).hover(function() {
+        $('.floe-resources .floe-resources-category a').css({ 'background-color': '#E6E6E6', 'color': '#000000' }).hover(function() {
           $(this).css({ 'background-color': '#000000', 'color': '#fccd00' });
           }).mouseout(function() {
           $(this).css({ 'background-color': '#E6E6E6', 'color': '#000000' });
@@ -26,13 +39,8 @@ $(document).ready(function(){
 
 
   // Toggle each category to show content
-  $('.widget .widget-title').click(function() {
-  $(this)
-  .addClass("open")
-  .parent().find('.widget-content').show();
+  $('.floe-resources-category').click(function() {
+    $('.floe-resources-content').toggleClass('floe-resources-content-show');
   });
-    $( ".floe-resources-toggle" ).click(function() {
-      $( "li" ).toggle();
-    });
 
 });
