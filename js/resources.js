040b5c3577f825a96138cba5acce95aad7d281b7
Eloisa <grrrero@users.noreply.github.com>
2018-02-27 12:06:56 -0500

    FLOE-540: Changed arrows on show/hide

--- js/resources.js
+++ js/resources.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 
   // Expand all
   $( '.floe-resources-show' ).click(function() {
+      $( '.arrow' ).text('\u25BC');
       $( '.floe-resources-content' ).show(function() {
-        $('.floe-resources .floe-resources-tab a').css({ 'background-color': '#fccd00', 'color': '#000000' });
-        $('.floe-resources .floe-resources-tab a').hover(function() {
+        $('.floe-resources .floe-resources-tab a').css({ 'background-color': '#fccd00', 'color': '#000000' }).hover(function() {
           $(this).css({ 'background-color': '#000000', 'color': '#fccd00' });
           }).mouseout(function() {
           $(this).css({ 'background-color': '#fccd00', 'color': '#000000' });
@@ -14,9 +14,9 @@ $(document).ready(function(){
 
   // Hide all
   $( '.floe-resources-hide' ).click(function() {
+      $( '.arrow' ).text('\u25B6');
       $( '.floe-resources-content' ).hide(function() {
-        $('.floe-resources .floe-resources-tab a').css({ 'background-color': '#E6E6E6', 'color': '#000000' });
-        $('.floe-resources .floe-resources-tab a').hover(function() {
+        $('.floe-resources .floe-resources-tab a').css({ 'background-color': '#E6E6E6', 'color': '#000000' }).hover(function() {
           $(this).css({ 'background-color': '#000000', 'color': '#fccd00' });
           }).mouseout(function() {
           $(this).css({ 'background-color': '#E6E6E6', 'color': '#000000' });
