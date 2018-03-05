9eeebdd7b33cc8677e33df1460787f4f817a44e7
Eloisa <grrrero@users.noreply.github.com>
2018-03-05 12:33:47 -0500

    FLOE-540: Refactored resources.js

--- js/resources.js
+++ js/resources.js
@@ -1,40 +1,29 @@
 $(document).ready(function(){
 
+  var toggleAll = function(elm, state) {
+    $( elm )
+        .attr('aria-expanded', state)
+        .closest('.floe-resources-category')
+        .find('div')
+        .toggleClass('floe-resources-content-show', state);
+  }
+
   // Expand all
   $( '.floe-resources-show' ).click(function() {
-      $( '.floe-resources-toggle' )
-        .attr('aria-expanded', true)
-        .closest('.floe-resources-category')
-        .find('div')
-        .toggleClass('floe-resources-content-show', true);
+      toggleAll('.floe-resources-toggle', true);
     });
 
-  // // Hide all
-    $( '.floe-resources-hide' ).click(function() {
-      $( '.floe-resources-toggle' )
-        .attr('aria-expanded', false)
-        .closest('.floe-resources-category')
-        .find('div')
-        .toggleClass('floe-resources-content-show', false);
+  // Hide all
+  $( '.floe-resources-hide' ).click(function() {
+      toggleAll('.floe-resources-toggle', false);
     });
 
   // Toggle each category to show content
   $('.floe-resources-toggle').click(function(evt) {
     var target = $(evt.delegateTarget);
     var state = target.attr('aria-expanded') === 'true' ? true : false;
-    target.attr('aria-expanded', !state)
-      .closest('.floe-resources-category')
-      .find('div')
-      .toggleClass('floe-resources-content-show', !state);
-      evt.preventDefault();
+    toggleAll(target, !state);
+    evt.preventDefault();
   });
 
 });
-
-
-    // if (!state) {
-    //   $(.arrow).text('\u25B6');
-    //   else {
-    //   $(.arrow).text('\u25BC');
-    //   }
-    // }
