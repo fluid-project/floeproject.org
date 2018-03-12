65d058a25732617bbcc811b9cbcc7cde5606d88c
grrrero <guerrewrong@gmail.com>
2018-03-12 11:24:20 -0400

    FLOE-540: Linted JS script with ESLint

--- js/resources.js
+++ js/resources.js
@@ -1,28 +1,31 @@
-$(document).ready(function(){
+/*global $ */
 
-  var toggleAll = function(elm, state) {
-    $( elm )
-        .attr('aria-expanded', state)
-        .closest('.floe-resources-category')
-        .toggleClass('floe-resources-content-show', state);
-  }
+$(document).ready(function () {
 
-  // Expand all
-  $( '.flc-resources-showAll' ).click(function() {
-      toggleAll('.flc-resources-toggleItem', true);
+    'use strict';
+    var toggleCategory = function (elm, state) {
+        $(elm)
+            .attr('aria-expanded', state)
+            .closest('.floe-resources-category')
+            .toggleClass('floe-resources-content-show', state);
+    };
+
+    // Expand all
+    $('.flc-resources-showAll').click(function () {
+        toggleCategory('.flc-resources-toggleItem', true);
     });
 
-  // Hide all
-  $( '.flc-resources-hideAll' ).click(function() {
-      toggleAll('.flc-resources-toggleItem', false);
+    // Hide all
+    $('.flc-resources-hideAll').click(function () {
+        toggleCategory('.flc-resources-toggleItem', false);
     });
 
-  // Toggle each category to show content
-  $('.flc-resources-toggleItem').click(function(evt) {
-    var target = $(evt.delegateTarget);
-    var state = target.attr('aria-expanded') === 'true' ? true : false;
-    toggleAll(target, !state);
-    evt.preventDefault();
-  });
+    // Toggle each category to show content
+    $('.flc-resources-toggleItem').click(function (evt) {
+        var target = $(evt.delegateTarget);
+        var state = target.attr('aria-expanded') === 'true' ? true : false;
+        toggleCategory(target, !state);
+        evt.preventDefault();
+    });
 
 });
