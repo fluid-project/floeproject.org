c427a0e43a5a2f9a03cb07f979e26028202ec813
sachin10101998 <schopra@mt.iitr.ac.in>
2020-08-05 21:52:14 +0530

    Added url.js to linter

--- src/assets/js/url.js
+++ src/assets/js/url.js
@@ -1,13 +1,7 @@
-var pathName = window.location.pathname;
-hostName= window.location.host;
-console.log("HI");
-console.log(hostName);
-console.log(pathName);
+"use strict";
+var hostName = window.location.host;
 var hrefSymLink = document.getElementsByClassName("hreff");
-console.log(hrefSymLink);
-console.log(hrefSymLink.length);
 for (var i = 0; i < hrefSymLink.length; i++) {
-    var init=hrefSymLink[i].getAttribute("href");
-    hrefSymLink[i].setAttribute("href","http://"+hostName+init);
-    console.log(hrefSymLink[i].getAttribute("href"));
-  }
+    var init = hrefSymLink[i].getAttribute("href");
+    hrefSymLink[i].setAttribute("href", "http://" + hostName + init);
+}
