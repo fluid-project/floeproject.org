e3921b4181e8566f08beea2ad88a77b4f4771778
sachin10101998 <schopra@mt.iitr.ac.in>
2020-08-07 22:59:11 +0530

    Changed sub directories to root directory for storing news articles

--- src/assets/js/url.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var hostName = window.location.host;
-var hrefSymLink = document.getElementsByClassName("hreff");
-for (var i = 0; i < hrefSymLink.length; i++) {
-    var init = hrefSymLink[i].getAttribute("href");
-    hrefSymLink[i].setAttribute("href", "http://" + hostName + init);
-}
