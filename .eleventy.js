290bcad22d75bdee28accbb90dcaa6bc91ba4e8a
Justin Obara <jobara@users.noreply.github.com>
2020-10-27 10:13:50 -0400

    ci: update due rename of default branch to main (#203)

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,12 @@
  Copyright the FLOE Project copyright holders.
 
  See the AUTHORS.md file at the top-level directory of this distribution and at
- https://github.com/fluid-project/floeproject.org/raw/master/AUTHORS.md.
+ https://github.com/fluid-project/floeproject.org/raw/main/AUTHORS.md.
 
  Licensed under the New BSD license. You may not use this file except in compliance with this License.
 
  You may obtain a copy of the New BSD License at
- https://github.com/fluid-project/floeproject.org/raw/master/LICENSE.md.
+ https://github.com/fluid-project/floeproject.org/raw/main/LICENSE.md.
 */
 
 const fs = require("fs");
--- src/assets/js/floe.js
+++ src/assets/js/floe.js
@@ -4,7 +4,7 @@ Licensed under the New BSD license. You may not use this file except in
 compliance with this License.
 
 See the AUTHORS.md file at the top-level directory of this distribution and at
-https://github.com/fluid-project/fluidproject.org/master/AUTHORS.md.
+https://github.com/fluid-project/fluidproject.org/main/AUTHORS.md.
 */
 /*global fluid */
 "use strict";
