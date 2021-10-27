9a069ee754633de3ff876fa737753cee6505533b
Justin Obara <obara.justin@gmail.com>
2021-10-27 16:36:45 -0400

    refactor: update to bring inline with current 11ty implmentations
    Note that this contains a linting error for news index. That fix will come in a future commit as the site needs to be restructured further.

--- .eleventy.js
+++ .eleventy.js
@@ -10,95 +10,72 @@
  https://github.com/fluid-project/floeproject.org/raw/main/LICENSE.md.
 */
 
+"use strict";
+
 const fs = require("fs");
 
-const rssPlugin = require('@11ty/eleventy-plugin-rss');
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const fluidPlugin = require("eleventy-plugin-fluid");
+const rssPlugin = require("@11ty/eleventy-plugin-rss");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 // Import filters
-const dateFilter = require('./src/filters/date-filter.js');
-const limitFilter = require('./src/filters/limit-filter.js');
-const markdownFilter = require('./src/filters/markdown-filter.js');
+const dateFilter = require("./src/filters/date-filter.js");
+const limitFilter = require("./src/filters/limit-filter.js");
+const markdownFilter = require("./src/filters/markdown-filter.js");
 
 // Import transforms
-const htmlMinTransform = require('./src/transforms/html-min-transform.js');
-const parseTransform = require('./src/transforms/parse-transform.js');
+const htmlMinTransform = require("./src/transforms/html-min-transform.js");
+const parseTransform = require("./src/transforms/parse-transform.js");
 
-// Import data files
-const site = require('./src/_data/site.json');
+module.exports = function (config) {
+    // Collections
+    config.addCollection("news", collection => {
+        return [
+            ...collection.getFilteredByGlob("src/news/*.md").sort((a, b) => b.data.date - a.data.date)
+        ].reverse();
+    });
 
-module.exports = function(config) {
-  // Collections
-  config.addCollection('news', collection => {
-    return [
-      ...collection.getFilteredByGlob('src/news/*.md').sort((a, b) => b.data.date - a.data.date)
-    ].reverse();
-  });
+    // Filters
+    config.addFilter("dateFilter", dateFilter);
+    config.addFilter("limit", limitFilter);
+    config.addFilter("markdownFilter", markdownFilter);
 
-  // Filters
-  config.addFilter('dateFilter', dateFilter);
-  config.addFilter('limit', limitFilter);
-  config.addFilter('markdownFilter', markdownFilter);
+    // Transforms
+    config.addTransform("htmlmin", htmlMinTransform);
+    config.addTransform("parse", parseTransform);
 
-  // Transforms
-  config.addTransform('htmlmin', htmlMinTransform);
-  config.addTransform('parse', parseTransform);
+    // Passthrough copy
+    config.addPassthroughCopy({"src/assets/images": "assets/images"});
+    config.addPassthroughCopy({"src/assets/stylesheets": "assets/stylesheets"}); // TODO: remove after updateing CSS
+    config.addPassthroughCopy({"src/news/images": "news/images"});
 
-  // Passthrough copy
-  config.addPassthroughCopy({"src/assets/images": "assets/images"});
-  config.addPassthroughCopy({"src/assets/js": "assets/js"});
-  config.addPassthroughCopy({"src/assets/stylesheets": "assets/stylesheets"});
-  config.addPassthroughCopy({"src/news/images": "news/images"});
-  config.addPassthroughCopy({"node_modules/infusion/dist/infusion-uio.min.js": "lib/infusion/infusion-uio.min.js"});
-  config.addPassthroughCopy({"node_modules/infusion/src/components/tableOfContents/css/": "lib/infusion/src/components/tableOfContents/css/"});
-  config.addPassthroughCopy({"node_modules/infusion/src/components/tableOfContents/html/": "lib/infusion/src/components/tableOfContents/html/"});
-  config.addPassthroughCopy({"node_modules/infusion/src/components/textfieldControl/css/": "lib/infusion/src/components/textfieldControl/css/"});
-  config.addPassthroughCopy({"node_modules/infusion/src/components/switch/css/": "lib/infusion/src/components/switch/css/"});
-  config.addPassthroughCopy({"node_modules/infusion/Infusion-LICENSE.txt": "lib/infusion/Infusion-LICENSE.txt"});
-  config.addPassthroughCopy({"node_modules/infusion/src/framework/preferences/css/": "lib/infusion/src/framework/preferences/css/"});
-  config.addPassthroughCopy({"node_modules/infusion/src/framework/preferences/fonts/": "lib/infusion/src/framework/preferences/fonts/"});
-  config.addPassthroughCopy({"node_modules/infusion/src/framework/preferences/html/": "lib/infusion/src/framework/preferences/html/"});
-  config.addPassthroughCopy({"node_modules/infusion/src/framework/preferences/messages/": "lib/infusion/src/framework/preferences/messages/"});
-  config.addPassthroughCopy({"node_modules/infusion/src/framework/preferences/preferencesDependencies.json": "lib/infusion/src/framework/preferences/preferencesDependencies.json"});
-  config.addPassthroughCopy({"node_modules/infusion/src/framework/enhancement/css/": "lib/infusion/src/framework/enhancement/css/"});
-  config.addPassthroughCopy({"node_modules/infusion/src/framework/core/css/": "lib/infusion/src/framework/core/css/"});
-  config.addPassthroughCopy({"node_modules/infusion/src/framework/core/images/": "lib/infusion/src/framework/core/images/"});
-  config.addPassthroughCopy({"node_modules/infusion/src/lib/jquery/ui/css/default-theme/": "lib/infusion/src/lib/jquery/ui/css/default-theme/"});
-  config.addPassthroughCopy({"node_modules/infusion/src/lib/normalize/css/": "lib/infusion/src/lib/normalize/css/"});
-  config.addPassthroughCopy({"node_modules/infusion/src/lib/open-dyslexic/fonts/": "lib/infusion/src/lib/open-dyslexic/fonts"});
-  config.addPassthroughCopy({"node_modules/infusion/src/lib/opensans/fonts/": "lib/infusion/src/lib/opensans/fonts/"});
-  config.addPassthroughCopy({"node_modules/infusion/src/lib/roboto/fonts/": "lib/infusion/src/lib/roboto/fonts/"});
+    // Plugins
+    config.addPlugin(fluidPlugin);
+    config.addPlugin(rssPlugin);
+    config.addPlugin(syntaxHighlight);
 
-  // Plugins
-  config.addPlugin(rssPlugin);
-  config.addPlugin(syntaxHighlight);
+    // 404
+    config.setBrowserSyncConfig({
+        callbacks: {
+            ready: function (err, bs) {
+                bs.addMiddleware("*", (req, res) => {
+                    const content_404 = fs.readFileSync("dist/404.html");
+                    // Provides the 404 content without redirect.
+                    res.write(content_404);
+                    res.writeHead(404);
+                    res.end();
+                });
+            }
+        }
+    });
 
-  // 404
-  config.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, bs) {
-
-        bs.addMiddleware("*", (req, res) => {
-          const content_404 = fs.readFileSync('dist/404.html');
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          // Add 404 http status code in request header.
-          // res.writeHead(404, { "Content-Type": "text/html" });
-          res.writeHead(404);
-          res.end();
-        });
-      }
-    }
-  });
-
-  return {
-    dir: {
-      input: 'src',
-      output: 'dist',
-      includes: "_includes"
-    },
-    htmlTemplateEngine: "njk",
-    	// 1.1 Enable elventy to pass dirs specified above
-    passthroughFileCopy: true
-  };
+    return {
+        dir: {
+            input: "src",
+            output: "dist",
+            includes: "_includes"
+        },
+        htmlTemplateEngine: "njk", // remove if all the templates are .njk files
+        passthroughFileCopy: true
+    };
 };
