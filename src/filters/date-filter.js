2c06636b80139ddddd89a74b7b1a83294d266e70
Justin Obara <obara.justin@gmail.com>
2021-10-27 20:14:40 -0400

    refactor: cleanup filters

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -3,9 +3,5 @@
 module.exports = function dateFilter(value) {
     const dateObject = new Date(value);
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const dayWithSuffix = dateObject.getUTCDate();
-    var month = `${months[dateObject.getMonth()]}`;
-    var day = ` ${dayWithSuffix}`;
-    var year = ` ${dateObject.getFullYear()}`;
-    return month + day + "," + year;
+    return `${months[dateObject.getMonth()]} ${dateObject.getUTCDate()}, ${dateObject.getFullYear()}`;
 };
