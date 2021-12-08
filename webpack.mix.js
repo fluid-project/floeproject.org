e6bed7faa1c88e85b44ddf4673bea7ad0afc3d07
Justin Obara <obara.justin@gmail.com>
2021-12-08 17:48:31 -0500

    feat: make entire news card clickable

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,6 +20,7 @@ mix.setPublicPath("dist/assets");
 
 // Process JavaScript files with Babel.
 mix.js("./src/assets/scripts/app.js", "dist/assets/scripts");
+mix.js("./src/assets/scripts/card.js", "dist/assets/scripts");
 mix.js("./src/assets/scripts/matomo.js", "dist/assets/scripts");
 
 // Process CSS with PostCSS.
